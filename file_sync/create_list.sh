052826eb71ab13eaee91251f39b532ee82ed0df4
Daniel Hiller <daniel.hiller.1972@gmail.com>
2017-10-16 09:19:25 +0200

    Add flatten files.
    - read parameters are now named
    - improve error handling

--- file_sync/create_list.sh
+++ file_sync/create_list.sh
@@ -1,13 +1,53 @@
 #!/bin/bash
+set -euo pipefail
+set -o posix
+IFS=$' \n\t'
 
 function usage {
-	echo "usage: $0 <directory> <file-extension>"
-	echo "$1"
+	echo "usage: $0 -d|--source-directory <directory> -f|--file-extension <file-extension> [-r|--remove-output-file]"
+	echo "       $0 -h|--help"
+	test "$1" != "" && echo "$1"
 	exit 1
 }
 
+# how to parse args: https://stackoverflow.com/a/14203146/16193
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+	key="$1"
 
-test -d "$1" || usage "'$1' is not a directory"
-test "$2" != "" || usage "'$2' is an empty string"
+	case $key in
+		-h|--help)
+		usage ""
+		;;
+		-d|--source-directory)
+		source_directory="$2"
+		test -d "$source_directory" || usage "source-directory '$source_directory' is not a directory"
+		shift
+		shift
+		;;
+		-f|--file-extension)
+		file_extension="$2"
+		test "$file_extension" != "" || usage "file-extension is an empty string"
+		shift
+		shift
+		;;
+		-r|--remove-output-file)
+		remove_output_file="yes"
+		shift
+		;;
+	    *)
+		usage ""
+	    ;;
+	esac
+done
 
-(find "$1" -name "*.$2" -type f | sed -e 's/[^/]*$//' | sort | uniq) >> "list_of_$2s.txt"
+output_file="list_of_${file_extension}s.txt"
+
+set +u
+test -z "$remove_output_file" || rm -f "$output_file" # touch "$output_file"
+set -u
+
+echo "Creating file list $output_file from $source_directory filtering for ${file_extension}s"
+
+(find "$source_directory" -name "*.$file_extension" -type f | sed -e 's/[^/]*$//' | sort | uniq) >> "$output_file"
--- file_sync/sync_files.sh
+++ file_sync/sync_files.sh
@@ -1,22 +1,76 @@
 #!/bin/bash
+set -euo pipefail
+set -o posix
+IFS=$'\n\t'
 
 function usage {
-	echo "usage: $0 <text-file-with-directories> <file-extension> <target-directory>"
-	echo "$1"
+	echo "usage: $0 [-f|--flatten] -i|--input-file <text-file-with-directories> -e|--file-extension <file-extension> -d|--target-directory <target-directory>"
+	echo "       $0 -h|--help"
+	test "$1" != "" && echo "$1"
 	exit 1
 }
 
-set -e
-
-test -f "$1" || usage "'$1' is not a file"
-test "$2" != "" || usage "'$2' is an empty string"
-test -d "$3" || usage "'$3' is not a directory"
-
-while read DIRECTORY
+# how to parse args: https://stackoverflow.com/a/14203146/16193
+POSITIONAL=()
+while [[ $# -gt 0 ]]
 do
-	TARGET_DIR="${DIRECTORY%/}"
-	TARGET_DIR="$3/${TARGET_DIR##*/}"
-	echo "Copying $2 from $DIRECTORY TO $TARGET_DIR"
-	mkdir -p "$TARGET_DIR"
-	find "$DIRECTORY" -name "*.$2" -type f -exec cp -n {} "${TARGET_DIR}/" \;
-done < "$1"
+	key="$1"
+
+	case $key in
+		-h|--help)
+		usage "help"
+		;;
+	    -f|--flatten)
+	    flatten="yes"
+	    shift
+	    ;;
+		-d|--source-directory)
+	    target_directory="$2"
+	    shift
+		shift
+	    ;;
+		-i|--input-file)
+	    input_file="$2"
+	    shift
+		shift
+	    ;;
+		-e|--file-extension)
+	    file_extension="$2"
+	    shift
+		shift
+	    ;;
+	    *)
+	    usage ""
+	    ;;
+	esac
+done
+
+set +u
+test -z "$flatten" && flatten=""
+test -f "$input_file" || usage "input file '$input_file' is not a file"
+test -z "$file_extension" && usage "file extension must not be empty"
+test -d "$target_directory" || usage "target directory '$target_directory' is not a directory"
+set -u
+
+while read directory
+do
+	target_dir="${directory%/}"
+	target_dir="$target_directory/${target_dir##*/}"
+	if [ -z "$flatten" ]; then
+		mkdir -p "$target_dir"
+	fi
+	echo "Base source: $directory"
+	for source_file in $(find "$directory" -name "*.$file_extension" -type f);
+	do
+		echo "Source: $source_file"
+		if [ -z "$flatten" ]; then
+			echo "Target: $target_dir/$source_file"
+			cp -n "$source_file" "${target_dir}/"
+		else
+			filename=$(basename "$source_file")
+			destination_filename="${target_dir} ${source_dir//\//-} - $filename"
+			echo "Target: $destination_filename"
+			cp -n "$source_file" "$destination_filename"
+		fi
+	done
+done < "$input_file"
