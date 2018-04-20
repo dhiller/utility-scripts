c7de0a1829ad9cb0837d2dabc2d5b25ad30a1911
Daniel Hiller <daniel.hiller.1972@gmail.com>
2018-04-20 08:24:26 +0200

    Suppress unnecessary output.

--- git/check_git_dirs.sh
+++ git/check_git_dirs.sh
@@ -5,20 +5,22 @@ CWD=$(pwd)
 for git_dir in $(find $(pwd) -name ".git" -type d -print);
 do
     cd "$(echo "${git_dir%.git}")"
-    echo "Checking $(pwd)"
+    git_dir=$(pwd)
 
     has_remote=$(git remote -v | wc -l)
-    if [ "$has_remote" -eq 0 ]; then
-        echo "- no remote set up"
-    fi
-
     has_uncommitted_changes=$(git status --porcelain | wc -l)
-    if [ "$has_uncommitted_changes" -ne 0 ]; then
-        echo "- uncommitted changes:"
-        git status --porcelain
-    fi
 
-    echo
+    if [ "$has_remote" -eq 0 ] || [ "$has_uncommitted_changes" -ne 0 ]; then
+        echo
+        echo "Checked $git_dir:"
+        if [ "$has_remote" -eq 0 ]; then
+            echo "- no remote set up"
+        fi
+        if [ "$has_uncommitted_changes" -ne 0 ]; then
+            echo "uncommitted changes:"
+            git status --porcelain
+        fi
+    fi
 done
 
 cd $CWD
