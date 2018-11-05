b2ee87d13bf8fb732d09cb67e302452f595db1c0
Daniel Hiller <daniel.hiller.1972@gmail.com>
2018-11-05 16:45:35 +0100

    minor fixes for svn_diff

--- svn/svn_display_diffs.sh
+++ svn/svn_display_diffs.sh
@@ -15,31 +15,35 @@ file_index_default=1
 while true; do
 	index=0
 	number_of_files=$(cat $diff_files | wc -l)
+	echo "Files stored in $diff_files:"
 	for diff_file in $(cat $diff_files); do
 		((index++))
 		printf '%3s: %s' "$index" "$diff_file"
 		echo
 	done
 
-	echo "Which diff file to display [$file_index_default] (e[x]it/[r]eset)? "
+	echo "Which diff file to display [$file_index_default] (e[x]it)? "
 	read input </dev/tty
 	case $input in
 		x)
 			exit 0
-			;;
-		r)
-			file_index_default=1
 			;;
 		*)
 			if [ ! -z $input ] && [[ $input -lt 1 || $input -gt $number_of_files ]]; then
 				echo "Line $input not found"
 			else
 				file_index=${input:-$file_index_default}
+				if [ ! -z $input ]; then
+					((file_index_default=file_index+1))
+				fi
 				diff_file=$(sed -n "$file_index p" $diff_files)
 				cat $diff_file | colordiff | less -r
 			fi
 			if [ -z $input ]; then
 				((file_index_default++))
+			fi
+			if [[ file_index_default -gt $number_of_files ]]; then
+				file_index_default=1
 			fi
 			;;
 	esac
