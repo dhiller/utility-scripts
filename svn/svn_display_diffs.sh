15e6dd839d6b3dd41922b31b489786ec460dee4d
Daniel Hiller <daniel.hiller.1972@gmail.com>
2018-11-06 09:56:00 +0100

    Display file list only once, and then on request

--- svn/svn_display_diffs.sh
+++ svn/svn_display_diffs.sh
@@ -4,6 +4,21 @@ function usage {
 	echo "Usage: $0 <file-containing-diff-files>"
 }
 
+function display_diff_files_numbered {
+	if [ ! -f $1 ]; then
+        	echo "File $1 not found!"
+	        exit 1
+	fi
+	diff_files="$1"
+        index=0
+        echo "Files stored in $diff_files:"
+        for diff_file in $(cat $diff_files); do
+                ((index++))
+                printf '%3s: %s' "$index" "$diff_file"
+                echo
+        done
+}
+
 if [ ! -f $1 ]; then
 	usage
 	exit 1
@@ -12,21 +27,18 @@ fi
 diff_files="$1"
 file_index_default=1
 # display files
-while true; do
-	index=0
-	number_of_files=$(cat $diff_files | wc -l)
-	echo "Files stored in $diff_files:"
-	for diff_file in $(cat $diff_files); do
-		((index++))
-		printf '%3s: %s' "$index" "$diff_file"
-		echo
-	done
+number_of_files=$(cat $diff_files | wc -l)
+display_diff_files_numbered "$diff_files"
 
-	echo "Which diff file to display [$file_index_default] (e[x]it)? "
+while true; do
+	echo "Which diff file to display [$file_index_default] (e[x]it/[d]isplay file list)? "
 	read input </dev/tty
 	case $input in
 		x)
 			exit 0
+			;;
+		d)
+			display_diff_files_numbered "$diff_files"
 			;;
 		*)
 			if [ ! -z $input ] && [[ $input -lt 1 || $input -gt $number_of_files ]]; then
