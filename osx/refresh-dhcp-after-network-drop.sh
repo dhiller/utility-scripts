ed22ff58afbf7db38f09b1404ce681e01204522e
Daniel Hiller <daniel.hiller.1972@gmail.com>
2019-01-30 18:29:33 +0100

    Add retry incremental, usage, passwrd support.

--- osx/refresh-dhcp-after-network-drop.sh
+++ osx/refresh-dhcp-after-network-drop.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function usage {
+  echo "${0##*/}"
+  echo ""
+  echo "Monitors network connection, resets network and refreshes DHCP lease if failure occurs"
+  echo "Accepts a password for use by sudo as parameter, however also evaluates environment variable REFRESH_DHCP_SUDO_PWD if set."
+  echo ""
+  echo "usage: ${0##*/} [<sudo_password>]"
+}
+
 function announce_sleep {
   local duration_seconds="$1"; shift
   echo "sleeping for $duration_seconds seconds"
@@ -14,8 +23,24 @@ function ping_dns {
   echo $result
 }
 
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+  usage
+  exit 0
+fi
+
+if [ $# -gt 0 ]; then
+  echo "using argument as sudo password"
+  password="$1"; shift
+else
+  if [ ! -z "$REFRESH_DHCP_SUDO_PWD" ]; then
+    echo "using password set by env var"
+    password="$REFRESH_DHCP_SUDO_PWD"
+  fi
+fi
+
 trap "{ exit 0; }" SIGINT SIGTERM
 
+network_resets=0
 while true; do
   result=$(ping_dns)
   if [ $result -ne 0 ]; then
@@ -23,13 +48,20 @@ while true; do
     echo "disabling network interface..."
     networksetup -setnetworkserviceenabled Wi-Fi off
     echo "renewing dhcp lease..."
-    sudo ipconfig set en0 DHCP
+    if [ ! -z $password ]; then
+      echo "$password" | sudo -S ipconfig set en0 DHCP
+    else
+      sudo ipconfig set en0 DHCP
+    fi
     announce_sleep 3
     echo "reenabling network interface..."
     networksetup -setnetworkserviceenabled Wi-Fi on
-    announce_sleep 5
+    ((network_resets++))
     retries=5
+    sleep_increase=1
+    sleep_duration=2
     while [ $retries -gt 0 ]; do
+      announce_sleep "$sleep_duration"
       echo "checking network packet"
       set +e
       ipconfig getpacket en0 > /dev/null
@@ -43,10 +75,12 @@ while true; do
           break
         fi
       fi
-      announce_sleep 3
+      sleep_duration=$((sleep_duration + sleep_increase))
       ((retries--))
     done
   else
+    clear
+    echo "network connection is alive ($network_resets network resets done)"
     sleep 3
   fi
 done
