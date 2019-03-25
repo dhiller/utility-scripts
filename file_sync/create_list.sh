c11765680303567e2395e8d878bcfd59f72cba89
Daniel Hiller <daniel.hiller.1972@gmail.com>
2019-03-25 22:17:41 +0100

    Consolidate file sync files.
    Consolidate file sync files into two scripts, one for creating a dir
    list, another one for copying files into a destination.ˆ

--- file_sync/create_list.sh
+++ file_sync/create_list.sh
@@ -4,10 +4,30 @@ set -o posix
 IFS=$' \n\t'
 
 function usage {
-	echo "usage: $0 -d|--source-directory <directory> -f|--file-extension <file-extension> [-r|--remove-output-file]"
-	echo "       $0 -h|--help"
-	test "$1" != "" && echo "$1"
-	exit 1
+    local script_name=$(basename $0)
+    cat <<EOF
+Usage: $script_name -d|--source-directory <source-dir>
+                    -f|--file-extension <file-extension>
+                    [-r|--remove-output-file]
+       $script_name -h|--help"
+
+Writes a list of directories containing files matching selected file extension
+from specified source-dir into output file \`list_of_<file-extension>s.txt\`. The
+list is sorted and unified.
+
+Parameters:
+    source-dir
+        The source directory to look for directories containing mp3
+        files
+
+    file-extension
+        The file extension of the files that the directories need
+        to contain
+
+    remove-output-file
+        Whether an existing output file should be deleted before
+EOF
+	test "$1" != "" && echo "$1"; exit 1 || exit 0
 }
 
 # how to parse args: https://stackoverflow.com/a/14203146/16193
@@ -22,13 +42,11 @@ do
 		;;
 		-d|--source-directory)
 		source_directory="$2"
-		test -d "$source_directory" || usage "source-directory '$source_directory' is not a directory"
 		shift
 		shift
 		;;
 		-f|--file-extension)
 		file_extension="$2"
-		test "$file_extension" != "" || usage "file-extension is an empty string"
 		shift
 		shift
 		;;
@@ -42,12 +60,15 @@ do
 	esac
 done
 
-output_file="list_of_${file_extension}s.txt"
-
 set +u
+test "$file_extension" != "" || usage "file-extension is an empty string"
+test -d "$source_directory" || usage "source-directory '$source_directory' is not a directory"
+set +e
+output_file="list_of_${file_extension}s.txt"
 test -z "$remove_output_file" || rm -f "$output_file" # touch "$output_file"
+set -e
 set -u
 
 echo "Creating file list $output_file from $source_directory filtering for ${file_extension}s"
 
-(find "$source_directory" -name "*.$file_extension" -type f | sed -e 's/[^/]*$//' | sort | uniq) >> "$output_file"
+find "$source_directory" -name "*.$file_extension" -type f | sed -e 's/[^/]*$//' | sort | uniq | tee -a "$output_file"
--- file_sync/sync_files.sh
+++ file_sync/sync_files.sh
@@ -4,10 +4,40 @@ set -o posix
 IFS=$'\n\t'
 
 function usage {
-	echo "usage: $0 [-f|--flatten] -i|--input-file <text-file-with-directories> -e|--file-extension <file-extension> -d|--target-directory <target-directory>"
-	echo "       $0 -h|--help"
-	test "$1" != "" && echo "$1"
-	exit 1
+    local script_name=$(basename $0)
+    cat <<EOF
+Usage: $0 -i|--input-file <text-file-with-directories>
+          -d|--target-directory <target-directory>
+          [-e|--file-extension <file-extension>]
+          [-n|--number-of-dirs <no-of-dirs>]
+          [-f|--flatten]
+       $0 -h|--help
+
+Copies files from specified number of directories to target
+directory.
+
+Directories are randomly selected using \`text-file-with-directories\` as
+input, where that file is expected to contain a list of directories.
+
+*Flattening* means that instead of keeping the source directory structure each
+file is copied directly into the destination folder.
+
+Parameters:
+    \`text-file-with-directories\`
+        The file containing the list of directories that contain the source
+        files
+
+    \`target-directory\`
+        the target directory where the source files should be copied
+        to
+
+    \`file-extension\` (optional, default:"mp3")
+        The file extension that determines which file type will be copied
+
+    \`no-of-dirs\` (optional, default:100)
+        The number of directories to copy files from
+EOF
+	test "$1" != "" && echo "$1"; exit 1 || exit 0
 }
 
 # how to parse args: https://stackoverflow.com/a/14203146/16193
@@ -16,9 +46,9 @@ while [[ $# -gt 0 ]]
 do
 	key="$1"
 
-	case $key in
+	case "$key" in
 		-h|--help)
-		usage "help"
+		usage ""
 		;;
 	    -f|--flatten)
 	    flatten="yes"
@@ -30,13 +60,18 @@ do
 		shift
 	    ;;
 		-i|--input-file)
-	    input_file="$2"
+	    source_file="$2"
 	    shift
 		shift
 	    ;;
 		-e|--file-extension)
 	    file_extension="$2"
 	    shift
+		shift
+	    ;;
+		-n|--number-of-dirs)
+		no_of_dirs="$2"
+		shift
 		shift
 	    ;;
 	    *)
@@ -46,31 +81,50 @@ do
 done
 
 set +u
+no_of_dirs="${no_of_dirs:-100}"
 test -z "$flatten" && flatten=""
-test -f "$input_file" || usage "input file '$input_file' is not a file"
-test -z "$file_extension" && usage "file extension must not be empty"
+test -f "$source_file" || usage "input file '$source_file' is not a file"
+test -z "$file_extension" || file_extension="mp3"
 test -d "$target_directory" || usage "target directory '$target_directory' is not a directory"
 set -u
 
-while read directory
+input_file=$(mktemp /tmp/source_mp3_dirs.XXXXXX)
+trap "rm -f ${input_file}" SIGINT SIGTERM EXIT
+set +e
+( cat  "$source_file" | grep -v "Misc" | gshuf | head -n "$no_of_dirs" | sort ) > "$input_file"
+set -e
+
+vi "$input_file"
+
+echo "Start process? [Y/n]"
+read input
+case "$input" in
+    n|no)
+        exit 0
+        ;;
+    *)
+        break
+        ;;
+esac
+
+echo "Copying files:"
+while read -r directory;
 do
-	target_dir="${directory%/}"
-	target_dir="$target_directory/${target_dir##*/}"
-	if [ -z "$flatten" ]; then
+    if [ ! -d "$directory" ]; then
+        echo "$directory does not exist! Skipping..."
+        continue
+    fi
+	target_dir="$( echo "$directory" | sed 's/^\(\/[^\/]*\)\{4\}\///g' | sed 's/\/$//g' | sed 's/\// - /g' )"
+	target_dir="$target_directory/$target_dir"
+	echo -e "\t$directory"
+	if [[ -z "$flatten" ]]; then
 		mkdir -p "$target_dir"
+		rsync -q -av --include='*/' --include="*.$file_extension" --exclude='*' "$directory/" "$target_dir/"
+    else
+        for source_file in $(find "$directory" -iname "*.$file_extension" -type f);
+        do
+            destination_filename="${target_dir} - $(basename "$source_file")"
+            rsync -q -av "$source_file" "$destination_filename"
+        done
 	fi
-	echo "Base source: $directory"
-	for source_file in $(find "$directory" -name "*.$file_extension" -type f);
-	do
-		echo "Source: $source_file"
-		if [ -z "$flatten" ]; then
-			echo "Target: $target_dir/$source_file"
-			cp -n "$source_file" "${target_dir}/"
-		else
-			filename=$(basename "$source_file")
-			destination_filename="${target_dir} ${source_dir//\//-} - $filename"
-			echo "Target: $destination_filename"
-			cp -n "$source_file" "$destination_filename"
-		fi
-	done
 done < "$input_file"
